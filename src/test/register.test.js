deada509cd0d575e3707afe211b03435fc114f6a
Arthur dubois <arthur.dbs70@gmail.com>
2023-02-03 08:10:10 +0100

    add mail in readme

--- src/test/register.test.js
+++ src/test/register.test.js
@@ -12,25 +12,29 @@ describe("RegisterForm", () => {
   it("no submit register form when data is not valid", () => {
     const onSubmit = jest.fn();
     render(
-		<Router>
-<Provider store={store}>
-		<RegisterForm />
-	</Provider>
-		</Router>
-	
-	);
+      <Router>
+        <Provider store={store}>
+          <RegisterForm />
+        </Provider>
+      </Router>
+    );
 
     const username = "testUsername";
     const email = "testEmail@test.com";
     const password = "test";
     const submitButton = screen.getByText("Register");
 
-	fireEvent.change(screen.getByLabelText("username"), { target: { value: username } });
-	fireEvent.change(screen.getByLabelText("emailuser"), { target: { value: email } });
-	fireEvent.change(screen.getByLabelText("password"), { target: { value: password } });
-	fireEvent.click(submitButton);
+    fireEvent.change(screen.getByLabelText("username"), {
+      target: { value: username },
+    });
+    fireEvent.change(screen.getByLabelText("emailuser"), {
+      target: { value: email },
+    });
+    fireEvent.change(screen.getByLabelText("password"), {
+      target: { value: password },
+    });
+    fireEvent.click(submitButton);
 
-	expect(onSubmit).not.toHaveBeenCalled();
+    expect(onSubmit).not.toHaveBeenCalled();
   });
 });
-
